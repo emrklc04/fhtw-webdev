bedeffd1839521845e22332e6f18c79d7e8cea7d
emre <kilicemre004@gmail.com>
2024-09-18 12:47:23 +0200

    help

--- help.php
+++ help.php
@@ -8,6 +8,40 @@
 
 <body>
     <h1>Anleitung</h1>
-    <h2></h2>
+ 
+
+    </h2>
     <h1>FAQ</h1>
+    <ul>
+            <h3>
+                <p>Registrierung</p>
+            </h3>
+            <li>Wie erstelle ich ein Benutzerkonto?</li>
+            <p>antwort auf die frage</p>
+            <hr>
+            <br>
+            <li>Wie aktualisiere ich meine Kontoinformationen?</li>
+            <p>antwort auf die frage</p>
+            <hr>
+            <br>
+            <h3>
+                <p>Such- und Filterfunktionen</p>
+            </h3>
+            <li>Wie kann ich nach speziellen Zimmern oder Angeboten suchen?</li>
+            <p>antwort auf die frage</p>
+            <hr>
+            <br>
+            <h3>
+                <p>Technische Probleme</p>
+            </h3>
+            <li>Was soll ich tun, wenn ich technische Probleme auf der Website habe?</li>
+            <p>antwort auf die frage</p>
+            <hr>
+            <br>
+            <li>Die Website lädt langsam. Was kann ich tun?</li>
+            <p>antwort auf die frage</p>
+            <hr>
+            <br>
+    </ul>
+
 </body>
